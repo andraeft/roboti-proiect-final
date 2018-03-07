e68acd1b325ab593dfaecdae24933b893cbfdfaa
andraeft <34285438+andraeft@users.noreply.github.com>
2018-03-07 14:58:32 +0200

    Add files via upload

---
+++ fantome.ino
@@ -0,0 +1,79 @@
+#define NUMREADINGS 15
+
+int senseLimit = 15; int probePin = 0; int val = 0;
+
+int LED1 = 2; 
+int LED2 = 3;
+int LED3 = 4; 
+int LED4 = 5; 
+int LED5 = 9; 
+//int LED6 = 7; 
+//int LED7 = 8;
+
+int buzzer = 13;
+
+int buzzerTone = 0;
+
+unsigned long previousMillis = 0; const long interval = 1000;
+
+int readings[NUMREADINGS]; 
+int index = 0; 
+int total = 0; 
+int average = 0;
+
+void setup() {
+  pinMode(2, OUTPUT); 
+  pinMode(3, OUTPUT); 
+  pinMode(4, OUTPUT); 
+  pinMode(5, OUTPUT); 
+  pinMode(6, OUTPUT); 
+  pinMode(7, OUTPUT); 
+  pinMode(8, OUTPUT); 
+  pinMode(13, OUTPUT);
+
+  Serial.begin(9600);
+
+  for (int i = 0; i < NUMREADINGS; i++) 
+    readings[i] = 0;
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  fantome();
+}
+
+
+void fantome() {
+
+    val = analogRead(probePin);
+    
+    if(val >= 1){
+    
+        val = constrain(val, 1, senseLimit); val = map(val, 1, senseLimit, 1, 1023);
+        
+        total -= readings[index]; readings[index] = val; total += readings[index]; index = (index + 1);
+        
+        if (index >= NUMREADINGS) index = 0;
+        
+        average = total / NUMREADINGS;
+        
+        if (average > 100) {digitalWrite(LED1, HIGH);} else {digitalWrite(LED1, LOW);}
+        
+        if (average > 200) {digitalWrite(LED2, HIGH);} else {digitalWrite(LED2, LOW);}
+        
+        if (average > 300) {digitalWrite(LED3, HIGH);} else {digitalWrite(LED3, LOW);}
+        
+        if (average > 400) {digitalWrite(LED4, HIGH);} else {digitalWrite(LED4, LOW);}
+        
+        if (average > 500) {digitalWrite(LED5, HIGH);} else {digitalWrite(LED5, LOW);}
+        
+     //   if (average > 600) {digitalWrite(LED6, HIGH);} else {digitalWrite(LED6, LOW);}
+        
+     //   if (average > 700) {digitalWrite(LED7, HIGH); tone(buzzer, 1000);} else {digitalWrite(LED7, LOW); noTone(buzzer);}
+        
+        Serial.println(val);
+        
+        } // use output to aid in calibrating 
+
+}
---
+++ radar.ino
@@ -0,0 +1,126 @@
+/* Sweep
+ by BARRAGAN <http://barraganstudio.com>
+ This example code is in the public domain.
+
+ modified 8 Nov 2013
+ by Scott Fitzgerald
+ http://www.arduino.cc/en/Tutorial/Sweep
+*/
+
+#include <Servo.h>
+#include "pitches.h"
+
+
+
+const int buttonPin = 8;     // the number of the pushbutton pin
+int buttonState = 0;         // variable for reading the pushbutton status
+Servo myservo;               // create servo object to control a servo
+                             // twelve servo objects can be created on most boards
+bool directie = 0;
+int pos = 0;                 // variable to store the servo position
+int trig=6;
+int echo=7;
+int duration=0;
+int dist=0;
+int limit = 50;
+                            // notes in the melody:
+int melody[] = {
+  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
+  };
+                            // note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+  4, 8, 8, 4, 4, 4, 4, 4
+};
+
+unsigned long cMillis;
+unsigned long prevMillis = 0;
+unsigned long prevMillis2 = 0;
+
+void setup() {
+  
+  myservo.attach(12);  // attaches the servo on pin 12 to the servo object
+  
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+
+  Serial.begin(9600);
+  
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  
+      motor();
+      use_sensor();
+      //fantome();
+      
+}
+
+
+void motor()
+{
+  buttonState = digitalRead(buttonPin);
+  Serial.println(buttonState);
+   
+  if (buttonState == HIGH) 
+  {
+   if (pos < 180 && directie == 0)
+     {
+       myservo.write(pos);
+       delay(15);
+       pos++;
+       
+     }
+     else
+     {
+       directie = 1;
+     }
+
+   if (pos > 0 && directie == 1)
+     {
+       myservo.write(pos);
+       delay(15);
+       pos--;
+     }
+     else
+     {
+       directie = 0;
+     }
+  }
+  else
+  {
+    myservo.write(90);
+  }
+
+}
+
+void play_sound()
+{
+  unsigned cMillis2 = millis();
+  tone(13, NOTE_B2, 30);
+  cMillis2 = millis();
+   //after pauseBetweenNotes, stop the tone playing:
+   if (cMillis2 - prevMillis2 >= 20)
+    {
+      noTone(13);
+      prevMillis2 = cMillis2;
+    }  
+}
+
+void use_sensor()
+{
+  digitalWrite(trig, LOW);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+  duration=pulseIn(echo, HIGH);
+  dist=duration/2/29;
+
+  if (dist <= limit)
+     play_sound();
+
+  Serial.println(dist);
+}
+
+
+
